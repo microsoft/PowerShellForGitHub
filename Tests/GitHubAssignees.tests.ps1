888ed7bcb8e950160700b5791491391f61a3235a
Pepe Rivera <joriv@microsoft.com>
2018-11-27 11:02:53 -0800

    add tests

--- Tests/GitHubAssignees.tests.ps1
+++ Tests/GitHubAssignees.tests.ps1
@@ -73,15 +73,48 @@ $configFile = New-TemporaryFile
 Backup-GitHubConfiguration -Path $configFile
 Reset-GitHubConfiguration
 
-Describe 'Adding and removing assignees to an issue' {
-    $repo = New-GitHubRepository -RepositoryName ([Guid]::NewGuid().Guid) -AutoInit
+$repo = New-GitHubRepository -RepositoryName ([Guid]::NewGuid().Guid) -AutoInit
+$issue = New-GitHubIssue -Uri $repo.svn_url -Title "Test issue"
 
-    Context 'Adding and removing an assignee' {
+Describe 'Getting a valid assignee' {
 
+    Context 'For getting a valid assignee' {
+        $assigneeList = @(Get-GitHubAssigneeList -Uri $repo.svn_url)
+
+        It 'Should have returned the one assignee' {
+            $assigneeList.Count | Should be 1
+        }
+
+        $assigneeUserName = $assigneeList[0].login
+        $hasPermission = Get-GithubAssigneePermissionCheck -Uri $repo.svn_url -Assignee $assigneeUserName
+
+        It 'Should have returned an assignee with permission to be assigned to an issue'{
+            $hasPermission | Should be $true
+        }
 
     }
-
-    Remove-GitHubRepository -Uri $repo.svn_url
 }
+
+Describe 'Adding and removing an assignee to an issue'{
+
+    Context 'For adding an assignee to an issue'{
+        $assignees = @($assigneeUserName)
+        Add-GithubAssignee -Uri $repo.svn_url -IssueNumber $issue.number -Assignees $assignees
+        $issue = Get-GitHubIssue -Uri $repo.svn_url -Issue $issue.number
+
+        It 'Should have assigned the user to the issue' {
+            $issue.assignee.login | Should be $assigneeUserName
+        }
+
+        Remove-GithubAssignee Uri $repo.svn_url -IssueNumber $issue.number -Assignees $assignees
+
+        It 'Should have removed the user from issue' {
+            $issue.assignees.Count | Should be 0
+        }
+    }
+}
+
+Remove-GitHubRepository -Uri $repo.svn_url
+
 # Restore the user's configuration to its pre-test state
 Restore-GitHubConfiguration -Path $configFile
