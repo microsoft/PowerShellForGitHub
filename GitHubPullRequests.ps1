1cb944752098e0e6d979db2d3d585cfa10efff4c
Robert Holt <rjmholt@gmail.com>
2019-05-20 23:07:14 -0700

    Add documentation

--- GitHubPullRequests.ps1
+++ GitHubPullRequests.ps1
@@ -168,51 +168,77 @@ function New-GitHubPullRequest
 { 
     <#
     .SYNOPSIS
-    Create a new pull request in the specified repository.
+        Create a new pull request in the specified repository.
     
     .DESCRIPTION
-    Opens a new pull request from the given branch into the given branch in the specified repository.
+        Opens a new pull request from the given branch into the given branch in the specified repository.
 
-    The Git repo for this module can be found here: http://aka.ms/PowerShellForGitHub
+        The Git repo for this module can be found here: http://aka.ms/PowerShellForGitHub
     
     .PARAMETER OwnerName
-    Parameter description
+        Owner of the repository.
+        If not supplied here, the DefaultOwnerName configuration property value will be used.
     
     .PARAMETER RepositoryName
-    Parameter description
+        Name of the repository.
+        If not supplied here, the DefaultRepositoryName configuration property value will be used.
     
     .PARAMETER Uri
-    Parameter description
+        Uri for the repository.
+        The OwnerName and RepositoryName will be extracted from here instead of needing to provide
+        them individually.
     
     .PARAMETER Title
-    Parameter description
+        The title of the pull request to be created.
     
-    .PARAMETER Head
-    Parameter description
+    .PARAMETER HeadBranch
+        The name of the head branch (the branch containing the changes to be merged).
+
+        May also include the name of the owner fork, in the form "${fork}:${branch}".
     
-    .PARAMETER Base
-    Parameter description
+    .PARAMETER BaseBranch
+        The name of the target branch of the pull request
+        (where the changes in the head will be merged to).
+
+    .PARAMETER HeadOwner
+        The name of fork that the change is coming from.
+        
+        Used as the prefix of $HeadBranch parameter in the form "${HeadOwner}:${HeadBranch}".
+
+        If unspecified, the unprefixed branch name is used,
+        creating a pull request from the $OwnerName fork of the repository.
     
     .PARAMETER Body
-    Parameter description
+        The text description of the pull request.
     
     .PARAMETER MaintainerCanModify
-    Parameter description
+        If set, allows repository maintainers to commit changes to the
+        head branch of this pull request.
     
     .PARAMETER Draft
-    Parameter description
+        If set, opens the pull request as a draft.
     
     .PARAMETER AccessToken
-    Parameter description
+        If provided, this will be used as the AccessToken for authentication with the
+        REST Api.  Otherwise, will attempt to use the configured value or will run unauthenticated.
     
     .PARAMETER NoStatus
-    Parameter description
+        If this switch is specified, long-running commands will run on the main thread
+        with no commandline status update.  When not specified, those commands run in
+        the background, enabling the command prompt to provide status information.
+        If not supplied here, the DefaultNoStatus configuration property value will be used.
+
+    .OUTPUTS
+        [PSCustomObject] An object describing the created pull request.
     
     .EXAMPLE
-    An example
-    
-    .NOTES
-    General notes
+        $prParams = @{
+            OwnerName = 'Microsoft'
+            Repository = 'PowerShellForGitHub'
+            Title = 'Add simple file to root'
+
+        }
+        $pr = New-GitHubPullRequest
     #>
     
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSShouldProcess", "", Justification="Methods called within here make use of PSShouldProcess, and the switch is passed on to them inherently.")]
@@ -233,10 +259,12 @@ function New-GitHubPullRequest
         [string] $Title,
 
         [Parameter(Mandatory)]
-        [string] $Head,
+        [string] $HeadBranch,
 
         [Parameter(Mandatory)]
-        [string] $Base,
+        [string] $BaseBranch,
+
+        [string] $HeadOwner,
 
         [string] $Body,
 
@@ -250,6 +278,21 @@ function New-GitHubPullRequest
     )
 
     Write-InvocationLog
+
+    if ($HeadOwner)
+    {
+        if ($HeadBranch.Contains(':'))
+        {
+            $message = "`$HeadBranch ('$HeadBranch') was specified with an owner prefix, but `$HeadOwner ('$HeadOwner') was also specified." +
+                " Either specify `$HeadBranch in '<owner>:<branch>' format, or set `$HeadBranch = '<branch>' and `$HeadOwner = '<owner>'."
+
+            Write-Log -Message $message -Level Error
+            throw $message
+        }
+
+        # $HeadBranch does not contain ':' - add the owner fork prefix
+        $HeadBranch = "${HeadOwner}:${HeadBranch}"
+    }
 
     $elements = Resolve-RepositoryElements
     $OwnerName = $elements.ownerName
@@ -265,8 +308,8 @@ function New-GitHubPullRequest
 
     $postBody = @{
         'title' = $Title
-        'head' = $Head
-        'base' = $Base
+        'head' = $HeadBranch
+        'base' = $BaseBranch
     }
 
     if ($Body)
