1eb72153672779d3112f3ead516e1f957ab7a849
Simon Heather <simonheather99@gmail.com>
2020-06-10 21:01:44 +0100

    Remove redundant Invocation parameters

--- GitHubTeams.ps1
+++ GitHubTeams.ps1
@@ -400,7 +400,7 @@ function New-GitHubTeam
         [switch] $NoStatus
     )
 
-    Write-InvocationLog -Invocation $MyInvocation
+    Write-InvocationLog
 
     $telemetryProperties = @{
         OrganizationName = (Get-PiiSafeString -PlainText $OrganizationName)
@@ -529,7 +529,7 @@ function Update-GitHubTeam
         [switch] $NoStatus
     )
 
-    Write-InvocationLog -Invocation $MyInvocation
+    Write-InvocationLog
 
     $telemetryProperties = @{
         OrganizationName = (Get-PiiSafeString -PlainText $OrganizationName)
@@ -692,7 +692,7 @@ function Remove-GitHubTeam
 
     if ($PSCmdlet.ShouldProcess($TeamName, "Remove Team"))
     {
-        Write-InvocationLog -Invocation $MyInvocation
+        Write-InvocationLog
 
         $params = @{
             UriFragment = $uriFragment
