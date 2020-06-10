8fe2884a8a133e28711cfa5979786bbfb38088d7
Howard Wolosky <HowardWolosky@users.noreply.github.com>
2020-06-10 00:56:51 -0700

    Making the fork tests robust to parallel execution
    We can't reliably test the number of additional forks that have been
    created of microsoft/PowerShellForGitHub at any given time, especially
    if there are parallel UT runs occurring.

--- Tests/GitHubRepositoryForks.tests.ps1
+++ Tests/GitHubRepositoryForks.tests.ps1
@@ -6,6 +6,11 @@
    Tests for GitHubRepositoryForks.ps1 module
 #>
 
+[CmdletBinding()]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '',
+    Justification='Suppress false positives in Pester code blocks')]
+param()
+
 # This is common test code setup logic for all Pester test files
 $moduleRootPath = Split-Path -Path $PSScriptRoot -Parent
 . (Join-Path -Path $moduleRootPath -ChildPath 'Tests\Common.ps1')
@@ -13,40 +18,38 @@ $moduleRootPath = Split-Path -Path $PSScriptRoot -Parent
 try
 {
     Describe 'Creating a new fork for user' {
-        $originalForks = @(Get-GitHubRepositoryFork -OwnerName Microsoft -RepositoryName PowerShellForGitHub)
+        BeforeEach {
+            $repo = New-GitHubRepositoryFork -OwnerName Microsoft -RepositoryName PowerShellForGitHub
+        }
+
+        AfterEach {
+            Remove-GitHubRepository -Uri $repo.svn_url -Confirm:$false
+        }
 
         Context 'When a new fork is created' {
-            $repo = New-GitHubRepositoryFork -OwnerName Microsoft -RepositoryName PowerShellForGitHub
             $newForks = @(Get-GitHubRepositoryFork -OwnerName Microsoft -RepositoryName PowerShellForGitHub -Sort Newest)
 
-            It 'Should have one more fork than before' {
-                ($newForks.Count - $originalForks.Count) | Should be 1
-            }
-
             It 'Should be the latest fork in the list' {
-                $newForks[0].full_name | Should be "$($script:ownerName)/PowerShellForGitHub"
+                $newForks.full_name | Should -Contain "$($script:ownerName)/PowerShellForGitHub"
             }
-
-            Remove-GitHubRepository -Uri $repo.svn_url -Confirm:$false
         }
     }
 
     Describe 'Creating a new fork for an org' {
-        $originalForks = @(Get-GitHubRepositoryFork -OwnerName Microsoft -RepositoryName PowerShellForGitHub)
+        BeforeEach {
+            $repo = New-GitHubRepositoryFork -OwnerName Microsoft -RepositoryName PowerShellForGitHub -OrganizationName $script:organizationName
+        }
+
+        AfterEach {
+            Remove-GitHubRepository -Uri $repo.svn_url -Confirm:$false
+        }
 
         Context 'When a new fork is created' {
-            $repo = New-GitHubRepositoryFork -OwnerName Microsoft -RepositoryName PowerShellForGitHub -OrganizationName $script:organizationName
             $newForks = @(Get-GitHubRepositoryFork -OwnerName Microsoft -RepositoryName PowerShellForGitHub -Sort Newest)
 
-            It 'Should have one more fork than before' {
-                ($newForks.Count - $originalForks.Count) | Should be 1
-            }
-
             It 'Should be the latest fork in the list' {
-                $newForks[0].full_name | Should be "$($script:organizationName)/PowerShellForGitHub"
+                $newForks.full_name | Should -Contain  "$($script:organizationName)/PowerShellForGitHub"
             }
-
-            Remove-GitHubRepository -Uri $repo.svn_url -Confirm:$false
         }
     }
 }
