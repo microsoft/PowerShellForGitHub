efb0f7a3947eab8631ad02f229d4400b15b84a69
Simon Heather <simonheather99@gmail.com>
2020-06-12 19:45:52 +0100

    Split long SuppressMessageAttribute lines

--- GitHubRepositories.ps1
+++ GitHubRepositories.ps1
@@ -658,8 +658,10 @@ filter Get-GitHubRepository
         SupportsShouldProcess,
         DefaultParameterSetName='AuthenticatedUser')]
     [OutputType({$script:GitHubRepositoryTypeName})]
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSShouldProcess", "", Justification="Methods called within here make use of PSShouldProcess, and the switch is passed on to them inherently.")]
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSReviewUnusedParameter", "", Justification="One or more parameters (like NoStatus) are only referenced by helper methods which get access to it from the stack via Get-Variable -Scope 1.")]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSShouldProcess", "",
+        Justification="Methods called within here make use of PSShouldProcess, and the switch is passed on to them inherently.")]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSReviewUnusedParameter", "",
+        Justification="One or more parameters (like NoStatus) are only referenced by helper methods which get access to it from the stack via Get-Variable -Scope 1.")]
     param(
         [Parameter(
             ValueFromPipelineByPropertyName,
