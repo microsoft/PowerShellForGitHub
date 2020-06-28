microsoft/PowerShellForGitHub

commit 17f6122d7812ee4001ce4bdf630429e711e45f7b

Adding complete Pipeline support to the entire module (#242)
# Description

Comprehensively adds pipeline support throughout the entire module

This change required examining every method, and in some cases a few breaking changes (noted below) had to be introduced in order to make support work consistently end-to-end.  UT's were added for every function, which resuled in a few bugs that were identified and fixed (also noted below).

## Breaking changes
### Unavoidable Breaks
 * `Get-GitHubRepositoryBranch`: `Name` parameter is now `BranchName`.
  > A `GitHub.Repository` object has a `name` property which is the name of the repo.  Piping in the repo object to this method would confuse it, making it search for a branch with the name of the repo, as opposed to the desired effect of just listing out all branches in the repo.

 * `*-GitHub*Label`: `Name` parameter is now `Label`.
  > Similar to above.
  > `Name` was too generic and was causing unintended conflicts when passing in certain objects over the pipeline (like a `GitHub.Repository`) which also has a `name` property, making it impossible to pipe in a repository to list all labels (since it was trying to query for a label in that repository named the same as the repo.

 * `*-GitHubLabel`: `Milestone` parameter is now `MilestoneNumber`.
  > A `GitHub.Issue` contains a `milestone` object property, and PowerShell was complaining that there was no way to convert that to an `[int64]` when an Issue was passed in via the pipeline.  The only way to avoid this was to use `MilestoneNumber` which is the name of the property we add to `GitHub.Milestone` objects, and it's what we alias to in all other methods that interact with milestones.

 * `*-GitHubIssue`: `Milestone` parameter is now `MilestoneNumber`.

UpdateCheck.ps1
@@ -58,7 +58,8 @@ function Invoke-UpdateCheck
         if ($state -eq 'Failed')
         {
             # We'll just assume we're up-to-date if we failed to check today.
-            Write-Log -Message '[$moduleName] update check failed for today (web request failed).  Assuming up-to-date.' -Level Verbose
+            $message = '[$moduleName] update check failed for today (web request failed).  Assuming up-to-date.'
+            Write-Log -Message $message -Level Verbose
             $script:HasLatestVersion = $true
 
             # Clear out the job info (even though we don't need the result)
@@ -81,22 +82,26 @@ function Invoke-UpdateCheck
                 $script:HasLatestVersion = $latestVersion -eq $moduleVersion
                 if ($script:HasLatestVersion)
                 {
-                    Write-Log "[$moduleName] update check complete.  Running latest version: $latestVersion" -Level Verbose
+                    $message = "[$moduleName] update check complete.  Running latest version: $latestVersion"
+                    Write-Log =Message $message -Level Verbose
                 }
                 elseif ($moduleVersion -gt $latestVersion)
                 {
-                    Write-Log "[$moduleName] update check complete.  This version ($moduleVersion) is newer than the latest published version ($latestVersion)." -Level Verbose
+                    $message = "[$moduleName] update check complete.  This version ($moduleVersion) is newer than the latest published version ($latestVersion)."
+                    Write-Log -Message $message -Level Verbose
                 }
                 else
                 {
-                    Write-Log "A newer version of $moduleName is available ($latestVersion).  Your current version is $moduleVersion.  Run 'Update-Module $moduleName' to get up-to-date." -Level Warning
+                    $message = "A newer version of $moduleName is available ($latestVersion).  Your current version is $moduleVersion.  Run 'Update-Module $moduleName' to get up-to-date."
+                    Write-Log -Message $message -Level Warning
                 }
             }
             catch
             {
                 # This could happen if the server returned back an invalid (non-XML) response for the request
                 # for some reason.
-                Write-Log -Message "[$moduleName] update check failed for today (invalid XML response).  Assuming up-to-date." -Level Verbose
+                $message = "[$moduleName] update check failed for today (invalid XML response).  Assuming up-to-date."
+                Write-Log -Message $message -Level Verbose
                 $script:HasLatestVersion = $true
             }
 

GitHubAnalytics.ps1
@@ -24,8 +24,12 @@ function Group-GitHubIssue
         The date property that should be inspected when determining which week grouping the issue
         if part of.
 
+    .INPUTS
+        GitHub.Issue
+
     .OUTPUTS
-        [PSCustomObject[]] Collection of issues and counts, by week, along with the total count of issues.
+        [PSCustomObject[]]
+        Collection of issues and counts, by week, along with the total count of issues.
 
     .EXAMPLE
         $issues = @()
@@ -90,8 +94,12 @@ function Group-GitHubIssue
             foreach ($week in $weekDates)
             {
                 $filteredIssues = @($Issue | Where-Object {
-                    (($DateType -eq 'Created') -and ($_.created_at -ge $week) -and ($_.created_at -le $endOfWeek)) -or
-                    (($DateType -eq 'Closed') -and ($_.closed_at -ge $week) -and ($_.closed_at -le $endOfWeek))
+                    (($DateType -eq 'Created') -and
+                     ($_.created_at -ge $week) -and
+                     ($_.created_at -le $endOfWeek)) -or
+                    (($DateType -eq 'Closed') -and
+                     ($_.closed_at -ge $week) -and
+                     ($_.closed_at -le $endOfWeek))
                 })
 
                 $endOfWeek = $week
@@ -143,6 +151,9 @@ function Group-GitHubPullRequest
     .PARAMETER DateType
         The date property that should be inspected when determining which week grouping the
         pull request if part of.
+
+    .INPUTS
+        GitHub.PullRequest
 
     .OUTPUTS
         [PSCustomObject[]] Collection of pull requests and counts, by week, along with the
@@ -211,8 +222,12 @@ function Group-GitHubPullRequest
             foreach ($week in $weekDates)
             {
                 $filteredPullRequests = @($PullRequest | Where-Object {
-                    (($DateType -eq 'Created') -and ($_.created_at -ge $week) -and ($_.created_at -le $endOfWeek)) -or
-                    (($DateType -eq 'Merged') -and ($_.merged_at -ge $week) -and ($_.merged_at -le $endOfWeek))
+                    (($DateType -eq 'Created') -and
+                     ($_.created_at -ge $week) -and
+                     ($_.created_at -le $endOfWeek)) -or
+                    (($DateType -eq 'Merged') -and
+                     ($_.merged_at -ge $week) -and
+                     ($_.merged_at -le $endOfWeek))
                 })
 
                 $endOfWeek = $week
@@ -265,6 +280,7 @@ function Get-WeekDate
         Get-WeekDate -Weeks 10
 #>
     [CmdletBinding()]
+    [OutputType([DateTime[]])]
     param(
         [ValidateRange(0, 10000)]
         [int] $Weeks = 12

GitHubBranches.ps1
@@ -1,7 +1,13 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-function Get-GitHubRepositoryBranch
+@{
+    GitHubBranchTypeName = 'GitHub.Branch'
+ }.GetEnumerator() | ForEach-Object {
+     Set-Variable -Scope Script -Option ReadOnly -Name $_.Key -Value $_.Value
+ }
+
+filter Get-GitHubRepositoryBranch
 {
 <#
     .SYNOPSIS
@@ -38,22 +44,60 @@ function Get-GitHubRepositoryBranch
         the background, enabling the command prompt to provide status information.
         If not supplied here, the DefaultNoStatus configuration property value will be used.
 
+    .INPUTS
+        GitHub.Branch
+        GitHub.Content
+        GitHub.Event
+        GitHub.Issue
+        GitHub.IssueComment
+        GitHub.Label
+        GitHub.Milestone
+        GitHub.PullRequest
+        GitHub.Project
+        GitHub.ProjectCard
+        GitHub.ProjectColumn
+        GitHub.Release
+        GitHub.Repository
+
     .OUTPUTS
-        [PSCustomObject[]] List of branches within the given repository.
+        GitHub.Branch
+        List of branches within the given repository.
 
     .EXAMPLE
-        Get-GitHubRepositoryBranch -OwnerName Microsoft -RepositoryName PowerShellForGitHub
+        Get-GitHubRepositoryBranch -OwnerName microsoft -RepositoryName PowerShellForGitHub
 
         Gets all branches for the specified repository.
 
     .EXAMPLE
-        Get-GitHubRepositoryBranch -Uri 'https://github.com/PowerShell/PowerShellForGitHub' -Name master
+        $repo = Get-GitHubRepository -OwnerName microsoft -RepositoryName PowerShellForGitHub
+        $repo | Get-GitHubRepositoryBranch
+
+        Gets all branches for the specified repository.
+
+    .EXAMPLE
+        Get-GitHubRepositoryBranch -Uri 'https://github.com/PowerShell/PowerShellForGitHub' -BranchName master
 
         Gets information only on the master branch for the specified repository.
+
+    .EXAMPLE
+        $repo = Get-GitHubRepository -OwnerName microsoft -RepositoryName PowerShellForGitHub
+        $repo | Get-GitHubRepositoryBranch -BranchName master
+
+        Gets information only on the master branch for the specified repository.
+
+    .EXAMPLE
+        $repo = Get-GitHubRepository -OwnerName microsoft -RepositoryName PowerShellForGitHub
+        $branch = $repo | Get-GitHubRepositoryBranch -BranchName master
+        $branch | Get-GitHubRepositoryBranch
+
+        Gets information only on the master branch for the specified repository, and then does it
+        again.  This tries to show some of the different types of objects you can pipe into this
+        function.
 #>
     [CmdletBinding(
         SupportsShouldProcess,
         DefaultParameterSetName='Elements')]
+    [OutputType({$script:GitHubBranchTypeName})]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSShouldProcess", "", Justification="Methods called within here make use of PSShouldProcess, and the switch is passed on to them inherently.")]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSReviewUnusedParameter", "", Justification="One or more parameters (like NoStatus) are only referenced by helper methods which get access to it from the stack via Get-Variable -Scope 1.")]
     [Alias('Get-GitHubBranch')]
@@ -66,10 +110,13 @@ function Get-GitHubRepositoryBranch
 
         [Parameter(
             Mandatory,
+            ValueFromPipelineByPropertyName,
             ParameterSetName='Uri')]
+        [Alias('RepositoryUrl')]
         [string] $Uri,
 
-        [string] $Name,
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string] $BranchName,
 
         [switch] $ProtectedOnly,
 
@@ -90,7 +137,7 @@ function Get-GitHubRepositoryBranch
     }
 
     $uriFragment = "repos/$OwnerName/$RepositoryName/branches"
-    if (-not [String]::IsNullOrEmpty($Name)) { $uriFragment = $uriFragment + "/$Name" }
+    if (-not [String]::IsNullOrEmpty($BranchName)) { $uriFragment = $uriFragment + "/$BranchName" }
 
     $getParams = @()
     if ($ProtectedOnly) { $getParams += 'protected=true' }
@@ -104,6 +151,54 @@ function Get-GitHubRepositoryBranch
         'NoStatus' = (Resolve-ParameterWithDefaultConfigurationValue -Name NoStatus -ConfigValueName DefaultNoStatus)
     }
 
-    return Invoke-GHRestMethodMultipleResult @params
+    return (Invoke-GHRestMethodMultipleResult @params | Add-GitHubBranchAdditionalProperties)
 }
 
+filter Add-GitHubBranchAdditionalProperties
+{
+<#
+    .SYNOPSIS
+        Adds type name and additional properties to ease pipelining to GitHub Branch objects.
+
+    .PARAMETER InputObject
+        The GitHub object to add additional properties to.
+
+    .PARAMETER TypeName
+        The type that should be assigned to the object.
+
+    .INPUTS
+        [PSCustomObject]
+
+    .OUTPUTS
+        GitHub.Branch
+#>
+    [CmdletBinding()]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "", Justification="Internal helper that is definitely adding more than one property.")]
+    param(
+        [Parameter(
+            Mandatory,
+            ValueFromPipeline)]
+        [AllowNull()]
+        [AllowEmptyCollection()]
+        [PSCustomObject[]] $InputObject,
+
+        [ValidateNotNullOrEmpty()]
+        [string] $TypeName = $script:GitHubBranchTypeName
+    )
+
+    foreach ($item in $InputObject)
+    {
+        $item.PSObject.TypeNames.Insert(0, $TypeName)
+
+        if (-not (Get-GitHubConfiguration -Name DisablePipelineSupport))
+        {
+            $elements = Split-GitHubUri -Uri $item.commit.url
+            $repositoryUrl = Join-GitHubUri @elements
+            Add-Member -InputObject $item -Name 'RepositoryUrl' -Value $repositoryUrl -MemberType NoteProperty -Force
+
+            Add-Member -InputObject $item -Name 'BranchName' -Value $item.name -MemberType NoteProperty -Force
+        }
+
+        Write-Output $item
+    }
+}

GitHubConfiguration.ps1
@@ -95,6 +95,13 @@ function Set-GitHubConfiguration
         Specify this switch to disable the hashing of potential PII data prior to submitting the
         data to telemetry (if telemetry hasn't been disabled via DisableTelemetry).
 
+    .PARAMETER DisablePipelineSupport
+        By default, this module will modify all objects returned by the API calls by adding
+        additional, consistent properties to those objects which ease pipelining those objects
+        into other functions.  This is highly convenient functionality.  You would only want to
+        disable this functionality if you are experiencing some edge case problems and are awaiting
+        a proper fix.
+
     .PARAMETER DisableSmarterObjects
         By default, this module will modify all objects returned by the API calls to update
         any properties that can be converted to objects (like strings for Date/Time's being
@@ -189,6 +196,8 @@ function Set-GitHubConfiguration
 
         [switch] $DisablePiiProtection,
 
+        [switch] $DisablePipelineSupport,
+
         [switch] $DisableSmarterObjects,
 
         [switch] $DisableTelemetry,
@@ -279,6 +288,7 @@ function Get-GitHubConfiguration
             'DefaultRepositoryName',
             'DisableLogging',
             'DisablePiiProtection',
+            'DisablePipelineSupport',
             'DisableSmarterObjects',
             'DisableTelemetry',
             'DisableUpdateCheck',
@@ -338,7 +348,8 @@ function Save-GitHubConfiguration
 
     if (($null -ne $ev) -and ($ev.Count -gt 0))
     {
-        Write-Log -Message "Failed to persist these updated settings to disk.  They will remain for this PowerShell session only." -Level Warning -Exception $ev[0]
+        $message = "Failed to persist these updated settings to disk.  They will remain for this PowerShell session only."
+        Write-Log -Message $message -Level Warning -Exception $ev[0]
     }
 }
 
@@ -449,7 +460,8 @@ function Resolve-PropertyValue
         }
         else
         {
-            Write-Log "The locally cached $Name configuration was not of type $Type.  Reverting to default value." -Level Warning
+            $message = "The locally cached $Name configuration was not of type $Type.  Reverting to default value."
+            Write-Log -Message $message -Level Warning
             return $DefaultValue
         }
     }
@@ -485,7 +497,8 @@ function Reset-GitHubConfiguration
         Deletes the local configuration file and loads in all default configuration values.
 
     .NOTES
-        This command will not clear your authentication token.  Please use Clear-GitHubAuthentication to accomplish that.
+        This command will not clear your authentication token.
+        Please use Clear-GitHubAuthentication to accomplish that.
 #>
     [CmdletBinding(SupportsShouldProcess)]
     param(
@@ -503,13 +516,15 @@ function Reset-GitHubConfiguration
 
         if (($null -ne $ev) -and ($ev.Count -gt 0) -and ($ev[0].FullyQualifiedErrorId -notlike 'PathNotFound*'))
         {
-            Write-Log -Message "Reset was unsuccessful.  Experienced a problem trying to remove the file [$script:configurationFilePath]." -Level Warning -Exception $ev[0]
+            $message = "Reset was unsuccessful.  Experienced a problem trying to remove the file [$script:configurationFilePath]."
+            Write-Log -Message $message -Level Warning -Exception $ev[0]
         }
     }
 
     Initialize-GitHubConfiguration
 
-    Write-Log -Message "This has not cleared your authentication token.  Call Clear-GitHubAuthentication to accomplish that." -Level Verbose
+    $message = "This has not cleared your authentication token.  Call Clear-GitHubAuthentication to accomplish that."
+    Write-Log -Message $message -Level Verbose
 }
 
 function Read-GitHubConfiguration
@@ -555,7 +570,8 @@ function Read-GitHubConfiguration
         }
         catch
         {
-            Write-Log -Message 'The configuration file for this module is in an invalid state.  Use Reset-GitHubConfiguration to recover.' -Level Warning
+            $message = 'The configuration file for this module is in an invalid state.  Use Reset-GitHubConfiguration to recover.'
+            Write-Log -Message $message -Level Warning
         }
     }
 
@@ -613,6 +629,7 @@ function Import-GitHubConfiguration
         'applicationInsightsKey' = '66d83c52-3070-489b-886b-09860e05e78a'
         'disableLogging' = ([String]::IsNullOrEmpty($logPath))
         'disablePiiProtection' = $false
+        'disablePipelineSupport' = $false
         'disableSmarterObjects' = $false
         'disableTelemetry' = $false
         'disableUpdateCheck' = $false
@@ -628,11 +645,13 @@ function Import-GitHubConfiguration
         'suppressTelemetryReminder' = $false
         'webRequestTimeoutSec' = 0
 
-        # This hash is generated by using Helper.ps1's Get-Sha512Hash in Tests/Config/Settings.ps1 like so:
+        # This hash is generated by using Helper.ps1's Get-Sha512Hash in Tests/Config/Settings.ps1
+        # like so:
         #    . ./Helpers.ps1; Get-Sha512Hash -PlainText (Get-Content -Path ./Tests/Config/Settings.ps1 -Raw -Encoding Utf8)
-        # The hash is used to identify if the user has made changes to the config file prior to running the UT's locally.
-        # It intentionally cannot be modified via Set-GitHubConfiguration and must be updated directly in the
-        # source code here should the default Settings.ps1 file ever be changed.
+        # The hash is used to identify if the user has made changes to the config file prior to
+        # running the UT's locally.  It intentionally cannot be modified via Set-GitHubConfiguration
+        # and must be updated directly in the source code here should the default Settings.ps1 file
+        # ever be changed.
         'testConfigSettingsHash' = '272EE14CED396100A7AFD23EA21CA262470B7F4D80E47B7ABD90508B86210775F020EEF79D322F4C22A53835F700E1DFD13D0509C1D08DD6F9771B3F0133EDAB'
     }
 
@@ -664,7 +683,7 @@ function Backup-GitHubConfiguration
         The path to store the user's current configuration file.
 
     .PARAMETER Force
-        If specified, will overwrite the contents of any file with the same name at th
+        If specified, will overwrite the contents of any file with the same name at the
         location specified by Path.
 
     .EXAMPLE
@@ -717,7 +736,9 @@ function Restore-GitHubConfiguration
     [CmdletBinding(SupportsShouldProcess)]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSShouldProcess", "", Justification="Methods called within here make use of PSShouldProcess, and the switch is passed on to them inherently.")]
     param(
-        [ValidateScript({if (Test-Path -Path $_ -PathType Leaf) { $true } else { throw "$_ does not exist." }})]
+        [ValidateScript({
+            if (Test-Path -Path $_ -PathType Leaf) { $true }
+            else { throw "$_ does not exist." }})]
         [string] $Path
     )
 
@@ -924,7 +945,8 @@ function Clear-GitHubAuthentication
         Clears out any GitHub API token from memory, as well as from local file storage.
 
     .NOTES
-        This command will not clear your configuration settings.  Please use Reset-GitHubConfiguration to accomplish that.
+        This command will not clear your configuration settings.
+        Please use Reset-GitHubConfiguration to accomplish that.
 #>
     [CmdletBinding(SupportsShouldProcess)]
     param(
@@ -946,14 +968,18 @@ function Clear-GitHubAuthentication
         {
             Remove-Item -Path $script:accessTokenFilePath -Force -ErrorAction SilentlyContinue -ErrorVariable ev
 
-            if (($null -ne $ev) -and ($ev.Count -gt 0) -and ($ev[0].FullyQualifiedErrorId -notlike 'PathNotFound*'))
+            if (($null -ne $ev) -and
+                ($ev.Count -gt 0) -and
+                ($ev[0].FullyQualifiedErrorId -notlike 'PathNotFound*'))
             {
-                Write-Log -Message "Experienced a problem trying to remove the file that persists the Access Token [$script:accessTokenFilePath]." -Level Warning -Exception $ev[0]
+                $message = "Experienced a problem trying to remove the file that persists the Access Token [$script:accessTokenFilePath]."
+                Write-Log -Message $message -Level Warning -Exception $ev[0]
             }
         }
     }
 
-    Write-Log -Message "This has not cleared your configuration settings.  Call Reset-GitHubConfiguration to accomplish that." -Level Verbose
+    $message = "This has not cleared your configuration settings.  Call Reset-GitHubConfiguration to accomplish that."
+    Write-Log -Message $message -Level Verbose
 }
 
 function Get-AccessToken
@@ -1010,19 +1036,22 @@ function Get-AccessToken
         {
             $secureString = $content | ConvertTo-SecureString
 
-            Write-Log -Message "Restoring Access Token from file.  This value can be cleared in the future by calling Clear-GitHubAuthentication." -Level Verbose
+            $message = "Restoring Access Token from file.  This value can be cleared in the future by calling Clear-GitHubAuthentication."
+            Write-Log -Message $messsage -Level Verbose
             $script:accessTokenCredential = New-Object System.Management.Automation.PSCredential "<username is ignored>", $secureString
             return $script:accessTokenCredential.GetNetworkCredential().Password
         }
         catch
         {
-           Write-Log -Message 'The Access Token file for this module contains an invalid SecureString (files can''t be shared by users or computers).  Use Set-GitHubAuthentication to update it.' -Level Warning
+           $message = 'The Access Token file for this module contains an invalid SecureString (files can''t be shared by users or computers).  Use Set-GitHubAuthentication to update it.'
+           Write-Log -Message $message -Level Warning
         }
     }
 
     if (-not [String]::IsNullOrEmpty($global:gitHubApiToken))
     {
-        Write-Log -Message 'Storing the Access Token in `$global:gitHubApiToken` is insecure and is no longer recommended.  To cache your Access Token for use across future PowerShell sessions, please use Set-GitHubAuthentication instead.' -Level Warning
+        $message = 'Storing the Access Token in `$global:gitHubApiToken` is insecure and is no longer recommended.  To cache your Access Token for use across future PowerShell sessions, please use Set-GitHubAuthentication instead.'
+        Write-Log -Message $message -Level Warning
         return $global:gitHubApiToken
     }
 
@@ -1030,7 +1059,8 @@ function Get-AccessToken
         (-not $script:seenTokenWarningThisSession))
     {
         $script:seenTokenWarningThisSession = $true
-        Write-Log -Message 'This module has not yet been configured with a personal GitHub Access token.  The module can still be used, but GitHub will limit your usage to 60 queries per hour.  You can get a GitHub API token from https://github.com/settings/tokens/new (provide a description and check any appropriate scopes).' -Level Warning
+        $message = 'This module has not yet been configured with a personal GitHub Access token.  The module can still be used, but GitHub will limit your usage to 60 queries per hour.  You can get a GitHub API token from https://github.com/settings/tokens/new (provide a description and check any appropriate scopes).'
+        Write-Log -Message $message -Level Warning
     }
 
     return $null

commit 1d16005551fc6823e23eca6577882a1021ded110

Invoke-UpdateCheck fixes
* Fixes a regression that was introduced in #242.  A `Write-Log`
  command was accidentally updated to pass in `=Message` instead of
  `-Message`.
* Updates the web request to suppress the progress bar using
  $ProgressPreference = 'SilentlyContinue'

Fixes #249
Fixes #250

UpdateCheck.ps1
@@ -26,13 +26,18 @@ function Invoke-UpdateCheck
 
         The Git repo for this module can be found here: http://aka.ms/PowerShellForGitHub
 
+    .PARAMETER Force
+        For debugging purposes, using this switch will allow the check to occur more than the limit
+        of once per day.  This _will not_ bypass the DisableUpdateCheck configuration value however.
+
     .EXAMPLE
         Invoke-UpdateCheck
 
     .NOTES
         Internal-only helper method.
 #>
-    param()
+    [cmdletbinding()]
+    param([switch] $Force)
 
     if (Get-GitHubConfiguration -Name DisableUpdateCheck)
     {
@@ -43,6 +48,18 @@ function Invoke-UpdateCheck
     $moduleVersion = $MyInvocation.MyCommand.Module.Version
 
     $jobNameToday = "Invoke-UpdateCheck-" + (Get-Date -format 'yyyyMMdd')
+
+    if ($Force)
+    {
+        if ($null -ne $script:UpdateCheckJobName)
+        {
+            # We're going to clear out the existing job and try running it again.
+            $null = Receive-Job -Name $script:UpdateCheckJobName -AutoRemoveJob -Wait -ErrorAction SilentlyContinue -ErrorVariable errorInfo
+        }
+
+        $script:UpdateCheckJobName = $null
+        $script:HasLatestVersion = $null
+    }
 
     # We only check once per day
     if ($jobNameToday -eq $script:UpdateCheckJobName)
@@ -83,7 +100,7 @@ function Invoke-UpdateCheck
                 if ($script:HasLatestVersion)
                 {
                     $message = "[$moduleName] update check complete.  Running latest version: $latestVersion"
-                    Write-Log =Message $message -Level Verbose
+                    Write-Log -Message $message -Level Verbose
                 }
                 elseif ($moduleVersion -gt $latestVersion)
                 {
@@ -132,6 +149,9 @@ function Invoke-UpdateCheck
 
         try
         {
+            # Disable Progress Bar in function scope during Invoke-WebRequest
+            $ProgressPreference = 'SilentlyContinue'
+
             Invoke-WebRequest @params
         }
         catch