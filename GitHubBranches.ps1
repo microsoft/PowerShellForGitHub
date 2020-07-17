3e30519c2a2d0f12aaf819519799020d1a81e0d3
Simon Heather <simonheather99@gmail.com>
2020-07-17 22:06:38 +0100

    Update

--- GitHubBranches.ps1
+++ GitHubBranches.ps1
@@ -100,7 +100,7 @@ filter Get-GitHubRepositoryBranch
     [CmdletBinding(
         SupportsShouldProcess,
         DefaultParameterSetName='Elements')]
-    [OutputType({$script:GitHubBranchTypeName })]
+    [OutputType({$script:GitHubBranchTypeName})]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSShouldProcess", "", Justification="Methods called within here make use of PSShouldProcess, and the switch is passed on to them inherently.")]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSReviewUnusedParameter", "", Justification="One or more parameters (like NoStatus) are only referenced by helper methods which get access to it from the stack via Get-Variable -Scope 1.")]
     [Alias('Get-GitHubBranch')]
