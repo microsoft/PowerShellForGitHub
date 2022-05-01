e90892378b4149947c63f39c1ce22f233122817b
tigerfansga <24516562+tigerfansga@users.noreply.github.com>
2022-05-01 16:23:40 +0000

    Update GitHubGistComments.tests.ps1 for Pester v5

--- Tests/GitHubGistComments.tests.ps1
+++ Tests/GitHubGistComments.tests.ps1
@@ -11,12 +11,12 @@
     Justification='Suppress false positives in Pester code blocks')]
 param()
 
-# This is common test code setup logic for all Pester test files
-$moduleRootPath = Split-Path -Path $PSScriptRoot -Parent
-. (Join-Path -Path $moduleRootPath -ChildPath 'Tests\Common.ps1')
+BeforeAll {
+    # This is common test code setup logic for all Pester test files
+    $moduleRootPath = Split-Path -Path $PSScriptRoot -Parent
+    . (Join-Path -Path $moduleRootPath -ChildPath 'Tests\Common.ps1')
+}
 
-try
-{
     Describe 'Get-GitHubGistComment' {
         BeforeAll {
             $body = 'Comment body'
@@ -32,13 +32,16 @@ try
                 $gist | Remove-GitHubGist -Force
             }
 
-            $comments = @(Get-GitHubGistComment -Gist $gist.id -MediaType 'Raw')
             It 'Should have no comments so far' {
+                $comments = @(Get-GitHubGistComment -Gist $gist.id -MediaType 'Raw')
                 $comments.Count | Should -Be 0
             }
 
-            $firstComment = New-GitHubGistComment -Gist $gist.id -Body $body
-            $comments = @(Get-GitHubGistComment -Gist $gist.id -MediaType 'Text')
+            Context 'Add comment to Gist' {
+                BeforeAll {
+                $firstComment = New-GitHubGistComment -Gist $gist.id -Body $body
+                $comments = @(Get-GitHubGistComment -Gist $gist.id -MediaType 'Text')
+                }
             It 'Should have one comments so far' {
                 $comments.Count | Should -Be 1
                 $comments[0].id | Should -Be $firstComment.id
@@ -54,8 +57,11 @@ try
                 $comments[0].user.PSObject.TypeNames[0] | Should -Be 'GitHub.User'
             }
 
+        Context 'Add second comment to gist' {
+            BeforeAll {
             $null = New-GitHubGistComment -Gist $gist.id -Body $body
             $comments = @(Get-GitHubGistComment -Gist $gist.id -MediaType 'Html')
+            }
             It 'Should have one comments so far' {
                 $comments.Count | Should -Be 2
                 foreach ($comment in $comments)
@@ -75,94 +81,13 @@ try
                     $comment.user.PSObject.TypeNames[0] | Should -Be 'GitHub.User'
                 }
             }
-
-            $comment = Get-GitHubGistComment -Gist $gist.id -Comment $firstComment.id -MediaType 'Html'
-            It 'Should retrieve the specific comment' {
-                $comment.id | Should -Be $firstComment.id
-                $comment.body | Should -BeNullOrEmpty
-                $comment.body_html | Should -Not -BeNullOrEmpty
-                $comment.body_text | Should -BeNullOrEmpty
-            }
-
-            It 'Should have the expected type and additional properties' {
-                $comment.PSObject.TypeNames[0] | Should -Be 'GitHub.GistComment'
-                $comment.GistCommentId | Should -Be $comment.id
-                $comment.GistId | Should -Be $gist.id
-                $comment.user.PSObject.TypeNames[0] | Should -Be 'GitHub.User'
-            }
         }
 
-        Context 'Gist on the pipeline' {
-            BeforeAll {
-                $gist = New-GitHubGist -FileName 'sample.txt' -Content 'Sample text'
-                $body = 'Comment body'
-            }
-
-            AfterAll {
-                $gist | Remove-GitHubGist -Force
-            }
-
-            $comments = @(Get-GitHubGistComment -Gist $gist.id -MediaType 'Text')
-            It 'Should have no comments so far' {
-                $comments.Count | Should -Be 0
-            }
-
-            $firstComment = $gist | New-GitHubGistComment -Body $body
-            $comments = @($gist | Get-GitHubGistComment -MediaType 'Raw')
-            It 'Should have one comments so far' {
-                $comments.Count | Should -Be 1
-                $comments[0].id | Should -Be $firstComment.id
-                $comments[0].body | Should -Not -BeNullOrEmpty
-                $comments[0].body_html | Should -BeNullOrEmpty
-                $comments[0].body_text | Should -BeNullOrEmpty
-            }
-
-            It 'Should have the expected type and additional properties' {
-                $comments[0].PSObject.TypeNames[0] | Should -Be 'GitHub.GistComment'
-                $comments[0].GistCommentId | Should -Be $comments[0].id
-                $comments[0].GistId | Should -Be $gist.id
-                $comments[0].user.PSObject.TypeNames[0] | Should -Be 'GitHub.User'
-            }
-
-            $null = $gist | New-GitHubGistComment -Body $body
-            $comments = @($gist | Get-GitHubGistComment -MediaType 'Full')
-            It 'Should have one comments so far' {
-                $comments.Count | Should -Be 2
-                foreach ($comment in $comments)
-                {
-                    $comment.body | Should -Not -BeNullOrEmpty
-                    $comment.body_html | Should -Not -BeNullOrEmpty
-                    $comment.body_text | Should -Not -BeNullOrEmpty
+            Context 'Retrieve a specific comment' {
+                BeforeAll {
+                $comment = Get-GitHubGistComment -Gist $gist.id -Comment $firstComment.id -MediaType 'Html'
                 }
-            }
-
-            It 'Should have the expected type and additional properties' {
-                foreach ($comment in $comments)
-                {
-                    $comment.PSObject.TypeNames[0] | Should -Be 'GitHub.GistComment'
-                    $comment.GistCommentId | Should -Be $comment.id
-                    $comment.GistId | Should -Be $gist.id
-                    $comment.user.PSObject.TypeNames[0] | Should -Be 'GitHub.User'
-                }
-            }
-
-            $comment = Get-GitHubGistComment -Gist $gist.id -Comment $firstComment.id -MediaType 'Html'
             It 'Should retrieve the specific comment' {
-                $comment.id | Should -Be $firstComment.id
-                $comment.body | Should -BeNullOrEmpty
-                $comment.body_html | Should -Not -BeNullOrEmpty
-                $comment.body_text | Should -BeNullOrEmpty
-            }
-
-            It 'Should have the expected type and additional properties' {
-                $comment.PSObject.TypeNames[0] | Should -Be 'GitHub.GistComment'
-                $comment.GistCommentId | Should -Be $comment.id
-                $comment.GistId | Should -Be $gist.id
-                $comment.user.PSObject.TypeNames[0] | Should -Be 'GitHub.User'
-            }
-
-            $comment = $firstComment | Get-GitHubGistComment -MediaType 'Html'
-            It 'Should retrieve the specific comment with the comment on the pipeline' {
                 $comment.id | Should -Be $firstComment.id
                 $comment.body | Should -BeNullOrEmpty
                 $comment.body_html | Should -Not -BeNullOrEmpty
@@ -177,6 +102,106 @@ try
             }
         }
     }
+        }
+
+        Context 'Gist on the pipeline' {
+            BeforeAll {
+                $gist = New-GitHubGist -FileName 'sample.txt' -Content 'Sample text'
+                $body = 'Comment body'
+            }
+
+            AfterAll {
+                $gist | Remove-GitHubGist -Force
+            }
+
+            It 'Should have no comments so far' {
+                $comments = @(Get-GitHubGistComment -Gist $gist.id -MediaType 'Text')
+                $comments.Count | Should -Be 0
+            }
+
+            Context 'Create first comment' {
+                BeforeAll {
+                $firstComment = $gist | New-GitHubGistComment -Body $body
+                }
+
+            It 'Should have one comments so far' {
+                $comments = @($gist | Get-GitHubGistComment -MediaType 'Raw')
+                $comments.Count | Should -Be 1
+                $comments[0].id | Should -Be $firstComment.id
+                $comments[0].body | Should -Not -BeNullOrEmpty
+                $comments[0].body_html | Should -BeNullOrEmpty
+                $comments[0].body_text | Should -BeNullOrEmpty
+            }
+
+            It 'Should have the expected type and additional properties' {
+                $comments = @($gist | Get-GitHubGistComment -MediaType 'Raw')
+                $comments[0].PSObject.TypeNames[0] | Should -Be 'GitHub.GistComment'
+                $comments[0].GistCommentId | Should -Be $comments[0].id
+                $comments[0].GistId | Should -Be $gist.id
+                $comments[0].user.PSObject.TypeNames[0] | Should -Be 'GitHub.User'
+            }
+
+            Context 'Add second comment' {
+                BeforeAll {
+                $null = $gist | New-GitHubGistComment -Body $body
+                }
+            It 'Should have one comments so far' {
+                $comments = @($gist | Get-GitHubGistComment -MediaType 'Full')
+                $comments.Count | Should -Be 2
+                foreach ($comment in $comments)
+                {
+                    $comment.body | Should -Not -BeNullOrEmpty
+                    $comment.body_html | Should -Not -BeNullOrEmpty
+                    $comment.body_text | Should -Not -BeNullOrEmpty
+                }
+            }
+
+            It 'Should have the expected type and additional properties' {
+                $comments = @($gist | Get-GitHubGistComment -MediaType 'Full')
+                foreach ($comment in $comments)
+                {
+                    $comment.PSObject.TypeNames[0] | Should -Be 'GitHub.GistComment'
+                    $comment.GistCommentId | Should -Be $comment.id
+                    $comment.GistId | Should -Be $gist.id
+                    $comment.user.PSObject.TypeNames[0] | Should -Be 'GitHub.User'
+                }
+            }
+        }
+
+            It 'Should retrieve the specific comment' {
+                $comment = Get-GitHubGistComment -Gist $gist.id -Comment $firstComment.id -MediaType 'Html'
+                $comment.id | Should -Be $firstComment.id
+                $comment.body | Should -BeNullOrEmpty
+                $comment.body_html | Should -Not -BeNullOrEmpty
+                $comment.body_text | Should -BeNullOrEmpty
+            }
+
+            It 'Should have the expected type and additional properties' {
+                $comment = Get-GitHubGistComment -Gist $gist.id -Comment $firstComment.id -MediaType 'Html'
+                $comment.PSObject.TypeNames[0] | Should -Be 'GitHub.GistComment'
+                $comment.GistCommentId | Should -Be $comment.id
+                $comment.GistId | Should -Be $gist.id
+                $comment.user.PSObject.TypeNames[0] | Should -Be 'GitHub.User'
+            }
+
+            It 'Should retrieve the specific comment with the comment on the pipeline' {
+                $comment = $firstComment | Get-GitHubGistComment -MediaType 'Html'
+                $comment.id | Should -Be $firstComment.id
+                $comment.body | Should -BeNullOrEmpty
+                $comment.body_html | Should -Not -BeNullOrEmpty
+                $comment.body_text | Should -BeNullOrEmpty
+            }
+
+            It 'Should have the expected type and additional properties' {
+                $comment = $firstComment | Get-GitHubGistComment -MediaType 'Html'
+                $comment.PSObject.TypeNames[0] | Should -Be 'GitHub.GistComment'
+                $comment.GistCommentId | Should -Be $comment.id
+                $comment.GistId | Should -Be $gist.id
+                $comment.user.PSObject.TypeNames[0] | Should -Be 'GitHub.User'
+            }
+        }
+    }
+}
 
     Describe 'New-GitHubGistComment' {
         BeforeAll {
@@ -189,7 +214,9 @@ try
         }
 
         Context 'By parameters' {
+            BeforeAll {
             $comment = New-GitHubGistComment -Gist $gist.id -Body $body
+            }
             It 'Should have the expected result' {
                 $comment.body | Should -Be $body
             }
@@ -203,7 +230,9 @@ try
         }
 
         Context 'Gist on the pipeline' {
+            BeforeAll {
             $comment = $gist | New-GitHubGistComment -Body $body
+            }
             It 'Should have the expected result' {
                 $comment.body | Should -Be $body
             }
@@ -229,17 +258,20 @@ try
         }
 
         Context 'By parameters' {
+            BeforeAll {
             $comment = New-GitHubGistComment -Gist $gist.id -Body $body
+            }
             It 'Should have the expected result' {
                 $comment.body | Should -Be $body
             }
 
-            $comment = Set-GitHubGistComment -Gist $gist.id -Comment $comment.id -Body $updatedBody -PassThru
             It 'Should have the expected result' {
+                $comment = Set-GitHubGistComment -Gist $gist.id -Comment $comment.id -Body $updatedBody -PassThru
                 $comment.body | Should -Be $updatedBody
             }
 
             It 'Should have the expected type and additional properties' {
+                $comment = Set-GitHubGistComment -Gist $gist.id -Comment $comment.id -Body $updatedBody -PassThru
                 $comment.PSObject.TypeNames[0] | Should -Be 'GitHub.GistComment'
                 $comment.GistCommentId | Should -Be $comment.id
                 $comment.GistId | Should -Be $gist.id
@@ -248,17 +280,20 @@ try
         }
 
         Context 'Gist on the pipeline' {
+            BeforeAll {
             $comment = $gist | New-GitHubGistComment -Body $body
+            }
             It 'Should have the expected result' {
                 $comment.body | Should -Be $body
             }
 
-            $comment = $gist | Set-GitHubGistComment -Comment $comment.id -Body $updatedBody -PassThru
             It 'Should have the expected result' {
+                $comment = $gist | Set-GitHubGistComment -Comment $comment.id -Body $updatedBody -PassThru
                 $comment.body | Should -Be $updatedBody
             }
 
             It 'Should have the expected type and additional properties' {
+                $comment = $gist | Set-GitHubGistComment -Comment $comment.id -Body $updatedBody -PassThru
                 $comment.PSObject.TypeNames[0] | Should -Be 'GitHub.GistComment'
                 $comment.GistCommentId | Should -Be $comment.id
                 $comment.GistId | Should -Be $gist.id
@@ -267,17 +302,20 @@ try
         }
 
         Context 'Gist Comment on the pipeline' {
+            BeforeAll {
             $comment = $gist | New-GitHubGistComment -Body $body
+            }
             It 'Should have the expected result' {
                 $comment.body | Should -Be $body
             }
 
-            $comment = $comment | Set-GitHubGistComment -Body $updatedBody -PassThru
             It 'Should have the expected result' {
+                $comment = $comment | Set-GitHubGistComment -Body $updatedBody -PassThru
                 $comment.body | Should -Be $updatedBody
             }
 
             It 'Should have the expected type and additional properties' {
+                $comment = $comment | Set-GitHubGistComment -Body $updatedBody -PassThru
                 $comment.PSObject.TypeNames[0] | Should -Be 'GitHub.GistComment'
                 $comment.GistCommentId | Should -Be $comment.id
                 $comment.GistId | Should -Be $gist.id
@@ -297,35 +335,34 @@ try
         }
 
         Context 'By parameters' {
-            $comment = New-GitHubGistComment -Gist $gist.id -Body $body
-
-            Remove-GitHubGistComment -Gist $gist.id -Comment $comment.id -Force
             It 'Should be gone' {
+                $comment = New-GitHubGistComment -Gist $gist.id -Body $body
+
+                Remove-GitHubGistComment -Gist $gist.id -Comment $comment.id -Force
                 { Get-GitHubGistComment -Gist $gist.id -Comment $comment.id } | Should -Throw
             }
         }
 
         Context 'Gist on the pipeline' {
-            $comment = $gist | New-GitHubGistComment -Body $body
-
-            $gist | Remove-GitHubGistComment -Comment $comment.id -Force
             It 'Should be gone' {
+                $comment = $gist | New-GitHubGistComment -Body $body
+
+                $gist | Remove-GitHubGistComment -Comment $comment.id -Force
                 { $gist | Get-GitHubGistComment -Comment $comment.id } | Should -Throw
             }
         }
 
         Context 'Gist Comment on the pipeline' {
-            $comment = $gist | New-GitHubGistComment -Body $body
-
-            $comment | Remove-GitHubGistComment -Force
             It 'Should be gone' {
+                $comment = $gist | New-GitHubGistComment -Body $body
+
+                $comment | Remove-GitHubGistComment -Force
                 { $comment | Get-GitHubGistComment } | Should -Throw
             }
         }
     }
-}
-finally
-{
+
+AfterAll {
     if (Test-Path -Path $script:originalConfigFile -PathType Leaf)
     {
         # Restore the user's configuration to its pre-test state
