f8b34640a2eb31afd0aecdf5b5180c83ebbb8b62
tigerfansga <24516562+tigerfansga@users.noreply.github.com>
2022-05-12 21:30:21 +0000

    Update GitHubMilestones.tests.ps1 for Pester v5

--- Tests/GitHubMilestones.tests.ps1
+++ Tests/GitHubMilestones.tests.ps1
@@ -11,19 +11,18 @@
     Justification='Suppress false positives in Pester code blocks')]
 param()
 
+BeforeAll {
 # This is common test code setup logic for all Pester test files
 $moduleRootPath = Split-Path -Path $PSScriptRoot -Parent
 . (Join-Path -Path $moduleRootPath -ChildPath 'Tests\Common.ps1')
 
-try
-{
     # Define Script-scoped, readonly, hidden variables.
     @{
         defaultMilestoneDueOn = (Get-Date).AddYears(1).ToUniversalTime()
     }.GetEnumerator() | ForEach-Object {
         Set-Variable -Force -Scope Script -Option ReadOnly -Visibility Private -Name $_.Key -Value $_.Value
     }
-
+}
     Describe 'Creating a milestone' {
         BeforeAll {
             $repo = New-GitHubRepository -RepositoryName ([Guid]::NewGuid().Guid) -AutoInit
@@ -50,9 +49,9 @@ try
                 $milestone | Remove-GitHubMilestone -Force
             }
 
-            $returned = Get-GitHubMilestone -OwnerName $repo.owner.login -RepositoryName $repo.name -Milestone $milestone.MilestoneNumber
-
             It 'Should exist' {
+                $returned = Get-GitHubMilestone -OwnerName $repo.owner.login -RepositoryName $repo.name -Milestone $milestone.MilestoneNumber
+
                 $returned.id | Should -Be $milestone.id
             }
 
@@ -84,9 +83,9 @@ try
                 $milestone | Remove-GitHubMilestone -Force
             }
 
-            $returned = $milestone | Get-GitHubMilestone
-
             It 'Should exist' {
+                $returned = $milestone | Get-GitHubMilestone
+
                 $returned.id | Should -Be $milestone.id
             }
 
@@ -118,9 +117,9 @@ try
                 $milestone | Remove-GitHubMilestone -Force
             }
 
-            $returned = $repo | Get-GitHubMilestone -Milestone $milestone.MilestoneNumber
-
             It 'Should exist' {
+                $returned = $repo | Get-GitHubMilestone -Milestone $milestone.MilestoneNumber
+
                 $returned.id | Should -Be $milestone.id
             }
 
@@ -144,8 +143,10 @@ try
         }
 
         Context 'That is due at different times of the day' {
+            BeforeAll {
             # We'll be testing to make sure that regardless of the time in the timestamp, we'll get the desired date.
             $title = 'Milestone title'
+            }
 
             It "Should have the expected due_on date even if early morning" {
                 $milestone = $repo | New-GitHubMilestone -Title 'Due early in the morning' -State "Closed" -DueOn $defaultMilestoneDueOn.date.AddHours(1)
@@ -182,30 +183,30 @@ try
                 $milestone.open_issues | Should -Be 0
             }
 
-            $issue = $issue | Set-GitHubIssue -Milestone $milestone.MilestoneNumber -PassThru
-            $milestone = $milestone | Get-GitHubMilestone
             It "Should be associated to the milestone now" {
+                $issue = $issue | Set-GitHubIssue -Milestone $milestone.MilestoneNumber -PassThru
+                $milestone = $milestone | Get-GitHubMilestone
                 $issue.milestone.number | Should -Be $milestone.MilestoneNumber
                 $milestone.open_issues | Should -Be 1
             }
 
-            $issue = $issue | Set-GitHubIssue -Milestone 0 -PassThru
-            $milestone = $milestone | Get-GitHubMilestone
             It 'Should no longer be associated to the milestone' {
+                $issue = $issue | Set-GitHubIssue -Milestone 0 -PassThru
+                $milestone = $milestone | Get-GitHubMilestone
                 $issue.milestone | Should -BeNullOrEmpty
                 $milestone.open_issues | Should -Be 0
             }
 
-            $issue = $issue | Set-GitHubIssue -Milestone $milestone.MilestoneNumber -PassThru
-            $milestone = $milestone | Get-GitHubMilestone
             It "Should be associated to the milestone again" {
+                $issue = $issue | Set-GitHubIssue -Milestone $milestone.MilestoneNumber -PassThru
+                $milestone = $milestone | Get-GitHubMilestone
                 $issue.milestone.number | Should -Be $milestone.MilestoneNumber
                 $milestone.open_issues | Should -Be 1
             }
 
-            $milestone | Remove-GitHubMilestone -Force
-            $issue = Get-GitHubIssue -Uri $repo.svn_url -Issue $issue.number
             It 'Should have removed the association when the milestone was deleted' {
+                $milestone | Remove-GitHubMilestone -Force
+                $issue = Get-GitHubIssue -Uri $repo.svn_url -Issue $issue.number
                 $issue.milestone | Should -BeNullOrEmpty
             }
         }
@@ -232,8 +233,11 @@ try
                 $openMilestone | Remove-GitHubMilestone -Force
             }
 
+            Context 'By parameter' {
+                BeforeAll {
             $milestone = $closedMilestone
             $returned = Get-GitHubMilestone -Uri $repo.RepositoryUrl -Milestone $milestone.MilestoneNumber
+        }
             It 'Should get the right milestone as a parameter' {
                 $returned.MilestoneId | Should -Be $milestone.MilestoneId
             }
@@ -245,9 +249,14 @@ try
                 $returned.MilestoneNumber | Should -Be $returned.number
                 $returned.creator.PSObject.TypeNames[0] | Should -Be 'GitHub.User'
             }
+}
 
+            Context 'By pipeline' {
+                BeforeAll {
             $milestone = $openMilestone
             $returned = $openMilestone | Get-GitHubMilestone
+        }
+
             It 'Should get the right milestone via the pipeline' {
                 $returned.MilestoneId | Should -Be $milestone.MilestoneId
             }
@@ -259,7 +268,7 @@ try
                 $returned.MilestoneNumber | Should -Be $returned.number
                 $returned.creator.PSObject.TypeNames[0] | Should -Be 'GitHub.User'
             }
-        }
+}        }
 
         Context 'Getting multiple milestones' {
             BeforeAll {
@@ -398,26 +407,25 @@ try
         }
 
         Context 'Using the parameter' {
+            It 'Should be deleted' {
             $milestone = $repo | New-GitHubMilestone -Title 'Milestone title' -State "Closed" -DueOn $defaultMilestoneDueOn
             Remove-GitHubMilestone -OwnerName $repo.owner.login -RepositoryName $repo.name -Milestone $milestone.MilestoneNumber -Force
 
-            It 'Should be deleted' {
                 { Get-GitHubMilestone -OwnerName $repo.owner.login -RepositoryName $repo.name -Milestone $milestone.MilestoneNumber } | Should -Throw
             }
         }
 
         Context 'Using the pipeline' {
+            It 'Should be deleted' {
             $milestone = $repo | New-GitHubMilestone -Title 'Milestone title' -State "Closed" -DueOn $defaultMilestoneDueOn
             $milestone | Remove-GitHubMilestone -Force
 
-            It 'Should be deleted' {
                 { $milestone | Get-GitHubMilestone } | Should -Throw
             }
         }
     }
-}
-finally
-{
+
+AfterAll {
     if (Test-Path -Path $script:originalConfigFile -PathType Leaf)
     {
         # Restore the user's configuration to its pre-test state
