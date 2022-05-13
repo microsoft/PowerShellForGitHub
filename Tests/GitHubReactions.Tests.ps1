a22277d66d52db7a16d0b0d5ef4671319f0206fb
tigerfansga <24516562+tigerfansga@users.noreply.github.com>
2022-05-13 03:02:23 +0000

    Update GitHubReactions.tests.ps1 for Pester v5

--- Tests/GitHubReactions.Tests.ps1
+++ Tests/GitHubReactions.Tests.ps1
@@ -11,12 +11,11 @@
     Justification='Suppress false positives in Pester code blocks')]
 param()
 
+BeforeAll {
 # This is common test code setup logic for all Pester test files
 $moduleRootPath = Split-Path -Path $PSScriptRoot -Parent
 . (Join-Path -Path $moduleRootPath -ChildPath 'Tests\Common.ps1')
 
-try
-{
     # Define Script-scoped, readonly, hidden variables.
     @{
         defaultIssueTitle = "Test Title"
@@ -26,6 +25,7 @@ try
     }.GetEnumerator() | ForEach-Object {
         Set-Variable -Force -Scope Script -Option ReadOnly -Visibility Private -Name $_.Key -Value $_.Value
     }
+}
 
     Describe 'Creating, modifying and deleting reactions' {
         BeforeAll {
@@ -35,18 +35,20 @@ try
         }
 
         Context 'For creating a reaction' {
+            It "Should have the expected reaction type" {
             Set-GitHubReaction -Uri $repo.svn_url -Issue $issue.IssueNumber -ReactionType $defaultReactionType
             $existingReaction = Get-GitHubReaction -Uri $repo.svn_url -Issue $issue.IssueNumber
 
-            It "Should have the expected reaction type" {
                 $existingReaction.content | Should -Be $defaultReactionType
             }
         }
 
         Context 'For getting reactions from an issue' {
+            BeforeAll {
             Get-GitHubIssue -Uri $repo.svn_url -Issue $issue.IssueNumber | Set-GitHubReaction -ReactionType $otherReactionType
             $allReactions = Get-GitHubReaction -Uri $repo.svn_url -Issue $issue.IssueNumber
             $specificReactions = Get-GitHubReaction -Uri $repo.svn_url -Issue $issue.IssueNumber -ReactionType $otherReactionType
+        }
 
             It 'Should have the expected number of reactions' {
                 $allReactions.Count | Should -Be 2
@@ -63,6 +65,7 @@ try
         }
 
         Context 'For getting reactions from a pull request' {
+            BeforeAll {
             # TODO: there are currently PRs out to add the ability to create new branches and add content to a repo.
             # When those go in, this test can be refactored to use those so the test is more reliable using a test PR.
             $url = 'https://github.com/microsoft/PowerShellForGitHub'
@@ -70,6 +73,7 @@ try
 
             $allReactions = $pr | Get-GitHubReaction
             $specificReactions = $pr | Get-GitHubReaction -ReactionType $otherReactionType
+        }
 
             It 'Should have the expected number of reactions' {
                 $allReactions.Count | Should -Be 2
@@ -86,17 +90,19 @@ try
         }
 
         Context 'For getting reactions from an Issue and deleting them' {
+            BeforeAll {
             $existingReactions = @(Get-GitHubReaction -Uri $repo.svn_url -Issue $issue.number)
+        }
 
             It 'Should have the expected number of reactions' {
                 $existingReactions.Count | Should -Be 2
             }
 
+            It 'Should have no reactions' {
             $existingReactions | Remove-GitHubReaction -Force
 
             $existingReactions = @(Get-GitHubReaction -Uri $repo.svn_url -Issue $issue.number)
 
-            It 'Should have no reactions' {
                 $existingReactions
                 $existingReactions.Count | Should -Be 0
             }
@@ -106,9 +112,8 @@ try
             Remove-GitHubRepository -Uri $repo.svn_url -Confirm:$false
         }
     }
-}
-finally
-{
+
+AfterAll {
     if (Test-Path -Path $script:originalConfigFile -PathType Leaf)
     {
         # Restore the user's configuration to its pre-test state
