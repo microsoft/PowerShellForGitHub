246d02b89dadb141b97c5524d740a409ede17309
X-Guardian <simonheather99@gmail.com>
2023-01-11 12:42:25 +0000

    Fix unit tests

--- Tests/GitHubRepositories.tests.ps1
+++ Tests/GitHubRepositories.tests.ps1
@@ -714,6 +714,27 @@ Describe 'GitHubRepositories\Set-GitHubRepository' {
             $repo = New-GitHubRepository -RepositoryName $repoName
         }
 
+        Context -Name 'When updating a public repository with Secret Scanning' {
+            BeforeAll -ScriptBlock {
+                $updateGithubRepositoryParms = @{
+                    OwnerName = $repo.owner.login
+                    RepositoryName = $repoName
+                    SecretScanning = 'Enabled'
+                }
+
+                $updatedRepo = Set-GitHubRepository @updateGithubRepositoryParms -PassThru
+            }
+
+            It 'Should return an object of the correct type' {
+                $updatedRepo | Should -BeOfType PSCustomObject
+            }
+
+            It 'Should return the correct properties' {
+                $updatedRepo.name | Should -Be $repoName
+                $updatedRepo.security_and_analysis.secret_scanning.status | Should -Be 'enabled'
+            }
+        }
+
         Context -Name 'When updating a repository with all possible settings' {
             BeforeAll -ScriptBlock {
                 $updateGithubRepositoryParms = @{
@@ -730,7 +751,6 @@ Describe 'GitHubRepositories\Set-GitHubRepository' {
                     DisallowRebaseMerge = $false
                     DeleteBranchOnMerge = $true
                     IsTemplate = $true
-                    SecretScanning = 'Enabled'
                 }
 
                 $updatedRepo = Set-GitHubRepository @updateGithubRepositoryParms -PassThru
@@ -753,7 +773,6 @@ Describe 'GitHubRepositories\Set-GitHubRepository' {
                 $updatedRepo.allow_rebase_merge | Should -BeTrue
                 $updatedRepo.delete_branch_on_merge | Should -BeTrue
                 $updatedRepo.is_template | Should -BeTrue
-                $updatedRepo.security_and_analysis.secret_scanning.status | Should -Be 'enabled'
             }
         }
 
